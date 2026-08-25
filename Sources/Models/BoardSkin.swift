dc636c8abf22edd594d2295f0eada997c4173017
Aimee1608 <shuigongqian@sina.com>
2026-08-25 14:29:32 +0800

    feat: 皮肤从2套扩到6套(果冻/钻石/星球/炫彩)
    参考用户发的同类游戏皮肤页(6套配色网格),照架构加了4套新配色,
    全部免费,不接金币购买。SkinPickerView 本身不用改,遍历
    BoardSkin.allCases 自动出新增的卡片。

--- Sources/Models/BoardSkin.swift
+++ Sources/Models/BoardSkin.swift
@@ -3,6 +3,10 @@ import SwiftUI
 enum BoardSkin: String, CaseIterable, Identifiable {
     case classic
     case light
+    case jelly
+    case diamond
+    case planet
+    case rainbow
 
     var id: String { rawValue }
 
@@ -10,6 +14,10 @@ enum BoardSkin: String, CaseIterable, Identifiable {
         switch self {
         case .classic: return "深绿"
         case .light: return "浅木"
+        case .jelly: return "果冻"
+        case .diamond: return "钻石"
+        case .planet: return "星球"
+        case .rainbow: return "炫彩"
         }
     }
 
@@ -19,6 +27,14 @@ enum BoardSkin: String, CaseIterable, Identifiable {
             return [Color(red: 0.14, green: 0.32, blue: 0.22), Color(red: 0.08, green: 0.22, blue: 0.15)]
         case .light:
             return [Color(red: 0.85, green: 0.72, blue: 0.55), Color(red: 0.72, green: 0.56, blue: 0.38)]
+        case .jelly:
+            return [Color(red: 0.96, green: 0.78, blue: 0.87), Color(red: 0.85, green: 0.66, blue: 0.92)]
+        case .diamond:
+            return [Color(red: 0.78, green: 0.9, blue: 0.97), Color(red: 0.55, green: 0.76, blue: 0.9)]
+        case .planet:
+            return [Color(red: 0.12, green: 0.08, blue: 0.26), Color(red: 0.05, green: 0.03, blue: 0.15)]
+        case .rainbow:
+            return [Color(red: 0.55, green: 0.25, blue: 0.75), Color(red: 0.32, green: 0.14, blue: 0.52)]
         }
     }
 
@@ -26,6 +42,10 @@ enum BoardSkin: String, CaseIterable, Identifiable {
         switch self {
         case .classic: return .black.opacity(0.3)
         case .light: return .black.opacity(0.18)
+        case .jelly: return .black.opacity(0.14)
+        case .diamond: return .black.opacity(0.14)
+        case .planet: return .white.opacity(0.12)
+        case .rainbow: return .black.opacity(0.22)
         }
     }
 
@@ -33,6 +53,10 @@ enum BoardSkin: String, CaseIterable, Identifiable {
         switch self {
         case .classic: return [Color(red: 0.2, green: 0.78, blue: 0.32), Color(red: 0.08, green: 0.5, blue: 0.18)]
         case .light: return [Color(red: 0.35, green: 0.62, blue: 0.32), Color(red: 0.2, green: 0.42, blue: 0.2)]
+        case .jelly: return [Color(red: 0.35, green: 0.88, blue: 0.78), Color(red: 0.15, green: 0.62, blue: 0.56)]
+        case .diamond: return [Color(red: 0.3, green: 0.58, blue: 0.98), Color(red: 0.12, green: 0.35, blue: 0.78)]
+        case .planet: return [Color(red: 0.25, green: 0.88, blue: 0.68), Color(red: 0.08, green: 0.58, blue: 0.42)]
+        case .rainbow: return [Color(red: 0.58, green: 0.95, blue: 0.28), Color(red: 0.38, green: 0.75, blue: 0.12)]
         }
     }
 
@@ -40,6 +64,10 @@ enum BoardSkin: String, CaseIterable, Identifiable {
         switch self {
         case .classic: return [Color(red: 1.0, green: 0.62, blue: 0.1), Color(red: 0.88, green: 0.38, blue: 0.0)]
         case .light: return [Color(red: 0.85, green: 0.42, blue: 0.2), Color(red: 0.65, green: 0.28, blue: 0.1)]
+        case .jelly: return [Color(red: 1.0, green: 0.48, blue: 0.68), Color(red: 0.88, green: 0.22, blue: 0.48)]
+        case .diamond: return [Color(red: 0.78, green: 0.72, blue: 0.95), Color(red: 0.58, green: 0.5, blue: 0.8)]
+        case .planet: return [Color(red: 1.0, green: 0.48, blue: 0.28), Color(red: 0.8, green: 0.22, blue: 0.1)]
+        case .rainbow: return [Color(red: 1.0, green: 0.28, blue: 0.68), Color(red: 0.85, green: 0.1, blue: 0.5)]
         }
     }
 }
